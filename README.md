e21eb7193ffcf1a6256c320f8eb842e8930d3e7e
ferasbinhussain <ferasbinhussain@gmail.com>
2017-09-15 13:52:53 +0300

    ..

---
+++ README.md
@@ -0,0 +1,16 @@
+## Neighborhood Map project
+* There are five of my favourite places in riyadh city filtered by list to show the users in google map 
+
+### Instructions
+
+* To view the website download all the files and open index.html in your browser .
+
+ 
+
+## What I used to complete this project 
+* My five favorate place in riyadh city.  
+* I used Knockout framwork.  
+* I used Foursquare API.
+* I used jquery. 
+
+
---
+++ js/app.js
@@ -0,0 +1,158 @@
+
+
+var initialLocations = [
+	{
+		name: 'Dunkin Donuts',
+		lat: 24.657961,
+		long: 46.694438
+	},
+	{
+		name: 'STARBUCKS',
+		lat: 24.663881,
+		long: 46.683205
+	},
+	{
+		name: 'AANI & DANI',
+		lat: 24.664095,
+		long:46.678899
+	},
+	{
+		name: 'Crown Prince Park',
+		lat: 24.711497,
+		long: 46.668265
+	},{
+		name: 'Al Sayad Seafood',
+		lat: 24.693112,
+		long: 46.676738
+	}
+
+];
+
+// Declaring global variables now to satisfy strict mode
+var map;
+var clientID;
+var clientSecret;
+
+
+
+
+var Location = function(data) {
+	var self = this;
+	this.name = data.name;
+	this.lat = data.lat;
+	this.long = data.long;
+	this.URL = "";
+	this.street = "";
+	this.city = "";
+	
+
+	this.visible = ko.observable(true);
+
+	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
+
+	$.getJSON(foursquareURL).done(function(data) {
+		var results = data.response.venues[0];
+		self.URL = results.url;
+		if (typeof self.URL === 'undefined'){
+			self.URL = "";
+		}
+		self.street = results.location.formattedAddress[0];
+     	self.city = results.location.formattedAddress[1];
+      
+	}).fail(function() {
+		alert("There was an error with the Foursquare API call.");
+	});
+
+	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" ;
+
+	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+
+	this.marker = new google.maps.Marker({
+			position: new google.maps.LatLng(data.lat, data.long),
+			map: map,
+			title: data.name
+	});
+
+	this.showMarker = ko.computed(function() {
+		if(this.visible() === true) {
+			this.marker.setMap(map);
+		} else {
+			this.marker.setMap(null);
+		}
+		return true;
+	}, this);
+
+	this.marker.addListener('click', function(){
+		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
+        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
+        '<div class="content">' + self.street + "</div>" +
+        '<div class="content">' + self.city + "</div>" ;
+
+        self.infoWindow.setContent(self.contentString);
+
+		self.infoWindow.open(map, this);
+
+		self.marker.setAnimation(google.maps.Animation.BOUNCE);
+      	setTimeout(function() {
+      		self.marker.setAnimation(null);
+     	}, 2100);
+	});
+
+	this.bounce = function(place) {
+		google.maps.event.trigger(self.marker, 'click');
+	};
+
+};
+
+function ViewModel() {
+	var self = this;
+
+	this.searchTerm = ko.observable("");
+
+	this.locationList = ko.observableArray([]);
+
+	map = new google.maps.Map(document.getElementById('map'), {
+			zoom: 10,
+			center: {lat: 24.774265, lng: 46.738586}
+	});
+
+	// Foursquare API settings
+	clientID = "0EY1SZOD313FCMX5YTRTQRQR5QFKBUIOE5VGYDEF0AASXG0B";
+	clientSecret = "2S03BMOG2BY0BTUJ2XOG5J2A22OZG0HX1ZQ3O0VTAPEKI0KQ";
+
+	initialLocations.forEach(function(locationItem){
+		self.locationList.push( new Location(locationItem));
+	});
+
+	this.filteredList = ko.computed( function() {
+		var filter = self.searchTerm().toLowerCase();
+		if (!filter) {
+			self.locationList().forEach(function(locationItem){
+				locationItem.visible(true);
+			});
+			return self.locationList();
+		} else {
+			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
+				var string = locationItem.name.toLowerCase();
+				var result = (string.search(filter) >= 0);
+				locationItem.visible(result);
+				return result;
+			});
+		}
+	}, self);
+
+
+	this.mapElem = document.getElementById('map');
+	this.mapElem.style.height = window.innerHeight - 50;
+}
+
+function startApp() {
+	ko.applyBindings(new ViewModel());
+}
+
+function errorHandling() {
+	alert("Google Maps has failed to load.");
+}
