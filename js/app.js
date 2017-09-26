cac7e7ec0fc44a3c9450230fb9a32b6eb1795678
ferasbinhussain <ferasbinhussain@gmail.com>
2017-09-26 15:19:20 +0300

    ..

--- js/app.js
+++ js/app.js
@@ -1,158 +1,135 @@
+var locations = [
+    {
+    	name: 'Dunkin Donuts',
+        lat: 24.657961,
+        long: 46.694438,
+        street: "King Fahd Road",
+        city: "Riyadh",
+        id: 0
+    },
+    {
+       name: 'STARBUCKS',
+        lat: 24.663881,
+		long: 46.683205,
+        street: "King Fahd Road",
+        city: "Riyadh", 
+        //visible: ko.observable(true),     
+        id: 1
+    },
+    {
+       name: 'AANI & DANI',
+        lat: 24.664095,
+		long:46.678899 ,
+        street: "Abdulrahman Alghafiqi St",
+        city: "Riyadh",
+        visible: ko.observable(true),
+        id: 2
+          },{
+        name: 'Crown Prince Park',
+        lat: 24.711497,
+		long: 46.668265,
+        street: "Almaather District",
+        city: "Riyadh",
+        //visible: ko.observable(true),
+        id: 3
+    },{
+       name: 'Al Sayad Seafood',
+        lat: 24.693112,
+		long: 46.676738,
+        street: "Tahlia St.",
+        city: "Riyadh",
+        //visible: ko.observable(true),
+        id: 4
+    }];
 
 
-var initialLocations = [
-	{
-		name: 'Dunkin Donuts',
-		lat: 24.657961,
-		long: 46.694438
-	},
-	{
-		name: 'STARBUCKS',
-		lat: 24.663881,
-		long: 46.683205
-	},
-	{
-		name: 'AANI & DANI',
-		lat: 24.664095,
-		long:46.678899
-	},
-	{
-		name: 'Crown Prince Park',
-		lat: 24.711497,
-		long: 46.668265
-	},{
-		name: 'Al Sayad Seafood',
-		lat: 24.693112,
-		long: 46.676738
-	}
-
-];
-
-// Declaring global variables now to satisfy strict mode
+// declaring global variable
 var map;
-var clientID;
-var clientSecret;
+
+var Location = function(data){
+	var self = this ;
+
+	self.name = ko.observable(data.name);
+	self.lat = ko.observable(data.lat);
+	self.lng = ko.observable(data.lng);
+	self.street = ko.observable(data.street);
+	self.city = ko.observable(data.city);
+	self.visible = ko.observable(true);
 
 
+	console.log(self.name());
 
+	self.contentString = '<div class="info-window-content"><div class="title"><b>' + self.name() + "</b></div>" +
+        '<div class="content">' + self.street() + "</div>" +
+        '<div class="content">' + self.city() + "</div>" ;
 
-var Location = function(data) {
-	var self = this;
-	this.name = data.name;
-	this.lat = data.lat;
-	this.long = data.long;
-	this.URL = "";
-	this.street = "";
-	this.city = "";
-	
+   this.infoWindow = new google.maps.InfoWindow({
+    content: self.contentString
+   	});
+   var marker = new google.maps.Marker({
+    position: new google.maps.LatLng(data.lat, data.long),
+    map: map,
+    title: data.name
+  });
+   
+  marker.addListener('click', function() {
+  	self.contentString = '<div class="info-window-content"><div class="title"><b>' + self.name() + "</b></div>" +
+        '<div class="content">' + self.street() + "</div>" +
+        '<div class="content">' + self.city() + "</div>" ;
+  	self.infoWindow.setContent(self.contentString);
 
-	this.visible = ko.observable(true);
+    self.infoWindow.open(map, this);
 
-	var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll='+ this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
-
-	$.getJSON(foursquareURL).done(function(data) {
-		var results = data.response.venues[0];
-		self.URL = results.url;
-		if (typeof self.URL === 'undefined'){
-			self.URL = "";
-		}
-		self.street = results.location.formattedAddress[0];
-     	self.city = results.location.formattedAddress[1];
-      
-	}).fail(function() {
-		alert("There was an error with the Foursquare API call.");
 	});
+  // link the list with the infoWindow 
+	 this.bounce = function(place) {
+		google.maps.event.trigger(marker, 'click');
+		console.log(place)
+	};
 
-	this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
-        '<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" ;
-
-	this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
-
-	this.marker = new google.maps.Marker({
-			position: new google.maps.LatLng(data.lat, data.long),
-			map: map,
-			title: data.name
-	});
-
-	this.showMarker = ko.computed(function() {
-		if(this.visible() === true) {
-			this.marker.setMap(map);
-		} else {
-			this.marker.setMap(null);
+  this.showMarker = ko.computed(function() {
+  	// loops in the locations to make the marker visible 
+		for (var i = 0; i > locations.length - 1; i++) {
+				if(locations[i].visible() === true) {
+					this.marker.setMap(map);
+				} else {
+					this.marker.setMap(null);
+				}
 		}
 		return true;
 	}, this);
-
-	this.marker.addListener('click', function(){
-		self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL +'">' + self.URL + "</a></div>" +
-        '<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" ;
-
-        self.infoWindow.setContent(self.contentString);
-
-		self.infoWindow.open(map, this);
-
-		self.marker.setAnimation(google.maps.Animation.BOUNCE);
-      	setTimeout(function() {
-      		self.marker.setAnimation(null);
-     	}, 2100);
-	});
-
-	this.bounce = function(place) {
-		google.maps.event.trigger(self.marker, 'click');
-	};
-
 };
-
-function ViewModel() {
-	var self = this;
-
-	this.searchTerm = ko.observable("");
-
+ function ViewModel() {
+ 	var self = this ;
+ 	
 	this.locationList = ko.observableArray([]);
 
 	map = new google.maps.Map(document.getElementById('map'), {
 			zoom: 10,
 			center: {lat: 24.774265, lng: 46.738586}
 	});
-
-	// Foursquare API settings
-	clientID = "0EY1SZOD313FCMX5YTRTQRQR5QFKBUIOE5VGYDEF0AASXG0B";
-	clientSecret = "2S03BMOG2BY0BTUJ2XOG5J2A22OZG0HX1ZQ3O0VTAPEKI0KQ";
-
-	initialLocations.forEach(function(locationItem){
+   
+	locations.forEach(function(locationItem){
 		self.locationList.push( new Location(locationItem));
+		console.log(locationItem);
 	});
-
-	this.filteredList = ko.computed( function() {
-		var filter = self.searchTerm().toLowerCase();
-		if (!filter) {
-			self.locationList().forEach(function(locationItem){
-				locationItem.visible(true);
+	
+    
+	// showimg the list  
+    this.filteredList = ko.computed( function() {
+            self.locationList().forEach(function(locationItem){
+				locationItem.visible();
 			});
-			return self.locationList();
-		} else {
-			return ko.utils.arrayFilter(self.locationList(), function(locationItem) {
-				var string = locationItem.name.toLowerCase();
-				var result = (string.search(filter) >= 0);
-				locationItem.visible(result);
-				return result;
-			});
-		}
-	}, self);
-
-
-	this.mapElem = document.getElementById('map');
-	this.mapElem.style.height = window.innerHeight - 50;
-}
-
+            return self.locationList();
+    }, self);
+ 
+   
+ }
+      
 function startApp() {
 	ko.applyBindings(new ViewModel());
 }
 
 function errorHandling() {
-	alert("Google Maps has failed to load.");
+	alert(" Google Maps has failed to load ");
 }
